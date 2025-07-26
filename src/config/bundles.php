c27e6bd8278794e62efc63c794200fb00242f5d0
Artem Naumov <naumovartem272@gmail.com>
2025-07-27 02:19:33 +0400

    Define base packages

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    A3Naumov\WebApiDriveBundle\A3NaumovWebApiDriveBundle::class => ['all' => true],
 ];
