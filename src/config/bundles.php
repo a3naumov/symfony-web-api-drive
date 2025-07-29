d2a3eba8b4001bfb798abb5b7d10b027bd137fdc
Artem Naumov <naumovartem272@gmail.com>
2025-07-30 03:19:07 +0400

    Install doctrine

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     A3Naumov\WebApiDriveBundle\A3NaumovWebApiDriveBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
