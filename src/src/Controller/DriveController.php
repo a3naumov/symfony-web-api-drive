94050e83b3be637c0688ea08a6a9e331adc05ebc
Artem Naumov <naumovartem272@gmail.com>
2025-08-03 03:08:22 +0400

    Add drive mapper

--- src/src/Controller/DriveController.php
+++ src/src/Controller/DriveController.php
@@ -7,7 +7,7 @@ namespace App\Controller;
 use App\Action\Drive\Create;
 use App\Action\Drive\Delete;
 use App\Api\Request\Drive\CreateRequest;
-use App\Api\Resource\DriveResource;
+use App\Mapper\DriveMapper;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,15 +19,13 @@ class DriveController
     public function create(
         #[MapRequestPayload] CreateRequest $request,
         Create $createDrive,
+        DriveMapper $mapper,
     ): JsonResponse {
         $drive = $createDrive->handle(
             name: $request->name,
         );
 
-        return new JsonResponse(['drives' => [new DriveResource(
-            id: $drive->getId(),
-            name: $drive->getName(),
-        )]], JsonResponse::HTTP_CREATED);
+        return new JsonResponse(['drives' => [$mapper->toApiResource($drive)]], JsonResponse::HTTP_CREATED);
     }
 
     #[Route(path: '/{id}', name: 'delete', methods: ['DELETE'])]
---
+++ src/src/Mapper/DriveMapper.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mapper;
+
+use A3Naumov\WebApiDriveCore\Application\Contract\Dto\DriveDtoInterface;
+use App\Api\Resource\DriveResource;
+
+class DriveMapper
+{
+    public function toApiResource(DriveDtoInterface $dto): DriveResource
+    {
+        return new DriveResource(
+            id: $dto->getId(),
+            name: $dto->getName(),
+        );
+    }
+}
